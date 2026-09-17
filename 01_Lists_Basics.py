34d57e49adc7add498eefcce59f61fbc59e687ef
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-17 23:24:19 +0500

    write code of first 2 part according to question

--- 01_Lists_Basics.py
+++ 01_Lists_Basics.py
@@ -17,3 +17,10 @@
 #     c) A mixed list (int, float, str, bool)
 #     d) An empty list
 #     e) A nested list (list inside a list)
+
+
+num = [10 , 20, 30, 40, 50]
+print(num, type(num))
+
+languages = ["Python", "C++", "Java"]
+print(languages, type(languages))
