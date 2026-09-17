8544471082f7d56863377f97416378afbebc7b40
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-17 23:20:09 +0500

    write first file details

---
+++ 01_Lists_Basics.py
@@ -0,0 +1,8 @@
+"""
+============================================
+  LECTURE 3 - FILE 1: LISTS BASICS & SLICING
+  Topics: List Creation, Access, Slicing (+/-)
+  Total Questions: 
+============================================
+
+"""
