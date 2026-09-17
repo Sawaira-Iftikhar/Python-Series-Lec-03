521588e8bd56cb5e48c8510246c6adbdf05887c9
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-17 23:22:16 +0500

    write Question 1 of Part A

--- 01_Lists_Basics.py
+++ 01_Lists_Basics.py
@@ -10,3 +10,10 @@
 # ==========================================
 #  PART A: LIST BASICS 
 # ==========================================
+
+# Q1. Create the following lists and print each with its type:
+#     a) A list of 5 integers
+#     b) A list of 3 strings (your favorite languages)
+#     c) A mixed list (int, float, str, bool)
+#     d) An empty list
+#     e) A nested list (list inside a list)
