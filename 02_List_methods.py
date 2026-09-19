1f4aff6460365ca8baf87eacb115653fbd8c8344
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-19 19:07:03 +0500

    Add details of second file

---
+++ 02_List_methods.py
@@ -0,0 +1,9 @@
+"""
+============================================
+  LECTURE 3 - FILE 2: LIST METHODS
+  Topics: append, insert, extend, remove,
+          pop, sort, reverse, count, index,
+          copy, clear
+  Total Questions: 
+============================================
+"""
