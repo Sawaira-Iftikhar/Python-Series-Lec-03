266ecacefee8e1164c87231864ee86c98147131a
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-20 01:28:04 +0500

    Q3 Count and Index Method

--- 03_Tuples.py
+++ 03_Tuples.py
@@ -92,3 +92,11 @@ OK! I Hope you get it.
 # ==========================================
 #  PART C: TUPLE METHODS 
 # ==========================================
+
+# Q3. COUNT & INDEX (The ONLY 2 tuple methods):
+#     Given: nums = (1, 2, 3, 2, 4, 2, 5, 2)
+#     a) Count how many times 2 appears → .count()
+#     b) Count how many times 9 appears → .count()
+#     c) Find the index of first 4 → .index()
+#     d) Find the index of first 2 → .index()
+#     e) What happens if you .index(99)? Write the error.
