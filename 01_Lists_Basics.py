138b370dc46de1415738b66fef510fc5eb65ece8
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-19 19:03:55 +0500

    write total Quesiton on top details. File completed

--- 01_Lists_Basics.py
+++ 01_Lists_Basics.py
@@ -2,7 +2,7 @@
 ============================================
   LECTURE 3 - FILE 1: LISTS BASICS & SLICING
   Topics: List Creation, Access, Slicing (+/-)
-  Total Questions: 
+  Total Questions: 8
 ============================================
 
 """
