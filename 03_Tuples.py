561478428e728048938c7aa243b4e39b95ed182a
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-20 00:57:20 +0500

    Q1 Creating Tuples

--- 03_Tuples.py
+++ 03_Tuples.py
@@ -9,3 +9,13 @@
 # ==========================================
 #  PART A: TUPLE BASICS 
 # ==========================================
+
+# Q1. CREATING TUPLES:
+#     Create and print the following tuples with their type:
+#     a) A tuple of 5 integers
+#     b) A tuple of 3 strings
+#     c) A mixed tuple (int, float, str, bool)
+#     d) An empty tuple
+#     e) A SINGLE element tuple (TRICKY! use a comma)
+#     f) A tuple WITHOUT parentheses (tuple packing)
+
