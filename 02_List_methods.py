11c132083574b5b58513bfe4af4f310e5510fe6c
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-19 19:19:30 +0500

    Q1 of Part A

--- 02_List_methods.py
+++ 02_List_methods.py
@@ -12,3 +12,10 @@
 #  PART A: ADDING ELEMENTS 
 # ==========================================
 
+# Q1. APPEND vs INSERT vs EXTEND:
+#     Given: nums = [1, 2, 3]
+#     a) Add 4 to the END  
+#     b) Add 99 at INDEX 0 
+#     c) Add [7, 8, 9] to the END 
+#     d) What happens if you .append([7, 8, 9]) instead of .extend()?
+#        Try it on a fresh list and explain the difference.
