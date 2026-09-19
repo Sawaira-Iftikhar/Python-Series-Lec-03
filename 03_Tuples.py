4e2470488bf1b9f1593c9c28c6a0b03770b91993
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-20 01:56:41 +0500

    add total number of questions 4 on top details file compeleted

--- 03_Tuples.py
+++ 03_Tuples.py
@@ -2,7 +2,7 @@
 ============================================
   LECTURE 3 - FILE 3: TUPLES
   Topics: Tuple Basics, Tuple Slicing, Tuple Methods
-  Total Questions: 
+  Total Questions: 4
 ============================================
 """
 
