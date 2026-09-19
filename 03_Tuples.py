977b0165fcda597d1d4b09be76d9306b42ecca96
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-20 00:54:48 +0500

    Add details of file 3 on the starting

---
+++ 03_Tuples.py
@@ -0,0 +1,7 @@
+"""
+============================================
+  LECTURE 3 - FILE 3: TUPLES
+  Topics: Tuple Basics, Tuple Slicing, Tuple Methods
+  Total Questions: 
+============================================
+"""
