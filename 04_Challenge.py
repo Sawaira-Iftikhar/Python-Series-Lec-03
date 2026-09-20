f89ab6bef16ace7104afaec8ff297799ed0d3237
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-20 05:48:29 +0500

    Add details related to file

---
+++ 04_Challenge.py
@@ -0,0 +1,11 @@
+"""
+============================================
+  LECTURE 3 - FILE 4: BOSS CHALLENGE 
+  Topics: ALL 6 Topics Combined
+  Total Challenges: 
+============================================
+  These problems combine Lists, Tuples,
+  Slicing, and Methods together.
+============================================
+"""
+
